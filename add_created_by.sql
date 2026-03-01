cb27f95fc80f630f6ede1dd0aba2025e1936f5d2
quantrinhansu123 <upedu2024@gmail.com>
2026-03-01 19:34:04 +0700

    cleanup: XÃ³a cÃ¡c file SQL script khÃ´ng cÃ²n sá»­ dá»¥ng

--- add_created_by.sql
+++
@@ -1,5 +0,0 @@
--- Add created_by column if it doesn't exist
-alter table orders add column if not exists created_by text;
-
--- Also ensuring 'cskh' column exists just in case
-alter table orders add column if not exists cskh text;
